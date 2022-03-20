fc0003cbdec8dcd8eb800428bd828c748ebaf594
MelikovRoman <77622004+MelikovRoman@users.noreply.github.com>
2022-03-20 16:44:55 +0300

    Правки

--- js/gen-photo.js
+++ js/gen-photo.js
@@ -5,14 +5,15 @@ const similarElemTemplate = document.querySelector('#picture').content;
 
 const similarListFragment = document.createDocumentFragment();
 
-massive.forEach(genMassiveData);
+massive.forEach((photoItem) => genMassiveData(photoItem));
 
 function genMassiveData(photoItem) {
   const photoItemElement = similarElemTemplate.cloneNode(true);
   photoItemElement.querySelector('.picture__likes').textContent = photoItem.likes;
   photoItemElement.querySelector('.picture__img').src = photoItem.url;
   photoItemElement.querySelector('.picture__comments').textContent = similarComment.length;
-  similarListElement.append(photoItemElement);
+  similarListFragment.append(photoItemElement);
 }
 
 similarListElement.append(similarListFragment);
+
