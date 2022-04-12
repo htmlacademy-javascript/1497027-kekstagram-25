b9db52e17081bb7288c84e2312ea9d74d39772f8
MelikovRoman <77622004+MelikovRoman@users.noreply.github.com>
2022-04-12 12:19:01 +0300

    Задание 7 часть 2

--- js/server-data.js
+++ js/server-data.js
@@ -1,10 +1,12 @@
 import {renderSimilarList} from './gen-photo.js';
 import {showAlertServerOff} from './util.js';
+import {openBigPicture} from './big-picture.js';
 
 fetch('https://25.javascript.pages.academy/kekstagram/data')
   .then((response) => response.json())
   .then((userPhotosServer) => {
     renderSimilarList(userPhotosServer);
+    openBigPicture();
   })
   .catch(() => {
     showAlertServerOff('Сервер временно недоступен');
