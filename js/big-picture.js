6622ee6e579f7770a0501a853aa97c6a3f599f91
MelikovRoman <77622004+MelikovRoman@users.noreply.github.com>
2022-04-17 02:52:18 +0300

    исправление бага и доработка задания

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,6 +4,9 @@ const closeBigPicture = document.querySelector('.big-picture__cancel');
 const similarElemTemplate = document.querySelector('.social__comment');
 const similarListElement = document.querySelector('.social__comments');
 const btnRenderMoreComments = document.querySelector('.social__comments-loader');
+let step = 0;
+const count = 5;
+let comments;
 
 function onPhotoEscKeydown (evt) {
   if (evt.key === 'Escape') {
@@ -20,8 +23,7 @@ function openBigPicture (value) {
       document.querySelector('body').classList.add('modal-open');
       const currentUserPhoto = value.find((item) => item.id === Number(currentElem.id));
       renderPhoto(currentUserPhoto);
-      const cloneArr = currentUserPhoto.comments.slice(0);
-      showComments(cloneArr);
+      showComments();
 
       document.addEventListener('keydown', onPhotoEscKeydown);
     }
@@ -42,31 +44,46 @@ function renderCommments (data) {
   }
 
   similarListElement.append(similarListFragment);
+
+  updateNumberComments();
 }
 
-function showComments (commentData) {
+function showComments () {
   similarListElement.innerHTML = '';
 
-  renderCommments(commentData.splice(0, 5));
+  addNewComment();
 
-  btnRenderMoreComments.addEventListener('click', () => {
-    const arrComments = commentData.splice(0, 5);
-    renderCommments(arrComments);
-  });
+  btnRenderMoreComments.addEventListener('click', addNewComment);
+}
+
+function addNewComment () {
+  renderCommments(comments.slice(step, step + count));
+  step += 5;
+}
+
+function updateNumberComments () {
+  const renderedComments = document.querySelectorAll('.social__comment');
+  document.querySelector('.social__comment-count').textContent = `${renderedComments.length  } из ${  comments.length  } комментариев`;
+
+  if (renderedComments.length === comments.length) {
+    btnRenderMoreComments.classList.add('hidden');
+  }
 }
 
 function renderPhoto (item) {
   document.querySelector('.social__caption').textContent = item.description;
   document.querySelector('.big-picture__img img').src = item.url;
   document.querySelector('.likes-count').textContent = item.likes;
-  document.querySelector('.comments-count').textContent = item.comments.length;
+  comments = item.comments.slice();
 }
 
 function closePhotoModal () {
   document.querySelector('.big-picture').classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
-
   document.removeEventListener('keydown', onPhotoEscKeydown);
+  btnRenderMoreComments.removeEventListener('click', addNewComment);
+  step = 0;
+  btnRenderMoreComments.classList.remove('hidden');
 }
 
 closeBigPicture.addEventListener('click', () => {
